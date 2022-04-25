afb97c12b176d66506697b34325f7130a3b14aa3
leat1027 <102127545+leat1027@users.noreply.github.com>
2022-04-25 16:42:01 -0700

    Update pwm.cpp
    Added changeDutyCycle for Auto mode needs tested.

--- AutoSunChair/src/pwm.cpp
+++ AutoSunChair/src/pwm.cpp
@@ -64,3 +64,12 @@ void changeDutyCycle(int adcNum){   // duty cycle  = pulse/period  = OCRnX/ICRn
     OCR3A = adcNum;
 
 }
+void changeDutyCycleAuto(int adcNum) {
+    float result = (adcNum * .97847)
+    int conversion = (int) result;
+        if((-512 < adcNum) && (0 >= adcNum)){  // toward 1 ms pulse (-90) 5% duty cycle
+               OCR3A = OCR3A  + conversion;
+}
+        else if((0 < adcNum) &&  ((511 >= adcNum)){ // toward 2 ms pulse (90) 10% duty cycle
+              OCR3A = OCR3A + conversion;
+}
